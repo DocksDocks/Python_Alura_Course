f0e9be223e576b3566e926b0d6ade1ca40be143c
DocksDocks <55303379+DocksDocks@users.noreply.github.com>
2022-08-07 01:24:28 -0300

    adjusting main

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import guess_game as gg
 import hangman_game as hang
 
-print("*********************************")
-print("        Choose your game!        ")
-print("*********************************")
 
-print("(1) Hangman (2) Guess Game")
+def chooseGame()
+    print("*********************************")
+    print("        Choose your game!        ")
+    print("*********************************")
+    print("(1) Hangman (2) Guess Game")
+    game = int(input("Which game would you like to play? "))
+    if (game == 1):
+        print("Starting Hangman")
+        hang.start()
+    elif (game == 2):
+        print("Starting Guess Game")
+        gg.start()
+        
 
-game = int(input("Which game would you like to play? "))
-
-if (game == 1):
-    print("Starting Hangman")
-    hang.start()
-elif (game == 2):
-    print("Starting Guess Game")
-    gg.start()
+if (__name__ == "__main__"):
+    chooseGame()
