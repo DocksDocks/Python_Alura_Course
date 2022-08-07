238bb77743c4cee188ccd9cf0cd581e39bd85ebb
DocksDocks <55303379+DocksDocks@users.noreply.github.com>
2022-08-07 19:28:03 -0300

    improvements hangman game

--- games/hangman_game.py
+++ games/hangman_game.py
@@ -1,28 +1,25 @@
 
-
-from re import A
-
-
 def start():
     print("**********************************")
     print("   Welcome to the Hangman Game!   ")
     print("**********************************")
 
-
-    secret_word = "banana"
+    secret_word = "Banana"
     hanged = False
     correct = False
     temp_word = '_'*len(secret_word)
+    listTemp = list(temp_word)
 
     while not hanged and not correct:
         letter_guess = input("Guess a letter of the word: ")
+        letter_guess = letter_guess.strip()
         
         for index,letter in enumerate(secret_word):
-            if(letter_guess == letter):
-                listTemp = list(temp_word)
-                listTemp[index] = letter_guess
+            if(letter_guess.lower() == letter.lower()):
+                listTemp[index] = letter
                 temp_word = ''.join(listTemp)
         print(temp_word)
         if(temp_word == secret_word):
             print("You won the game!")
             break
+        
